5a719712d83935e2d82c33663b677eb103e7e772
Kobe Ponet <kobe.ponet@student.kdg.be>
2024-04-16 10:54:23 +0200

    fixed bug

--- Domain/Questions/RangeQuestion.cs
+++ Domain/Questions/RangeQuestion.cs
@@ -9,7 +9,7 @@ public class RangeQuestion : Question
     {
     }
     
-    public RangeQuestion(string text, int min, int max) : base(text, QuestionType.RangeEnum)
+    public RangeQuestion(string text, int min, int max) : base(text, QuestionType.Range)
     {
         Min = min;
         Max = max;
