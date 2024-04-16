1da0a1834e8ae1aa920057c279bdce4c4a746f0f
Pepijn Theuns <pepijn.theuns@student.kdg.be>
2024-04-16 15:46:31 +0200

    webpack aangepast voor typescript en sass

--- IP_MVC/ClientApp/webpack.config.js
+++ IP_MVC/ClientApp/webpack.config.js
@@ -4,27 +4,40 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
-        site: './src/js/site.js',
-        bootstrap_js: './src/js/bootstrap_js.js',
-        validation: './src/js/validation.js',
-        index: './src/js/index.js'
+        site: './src/ts/site.ts',
+        validation: './src/ts/validation.ts',
+        index: './src/ts/index.ts'
     },
     output: {
         filename: '[name].entry.js',
-        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist'),
+        clean: true
     },
     devtool: 'source-map',
     mode: 'development',
+    resolve: {
+      extensions: [".ts", ".js"],
+      extensionAlias: {'.js': ['.js', '.ts']}
+    },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader']
+                test: /\.ts$/i,
+                use: ['ts-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset'
             },
             {
                 test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
                 type: 'asset'
-            },
+            }
         ]
     },
     plugins: [
