88d45c8341dc6a2f3e06a1cb08fce50717edbb47
Stefaan.Vos <stefaan.vos@student.kdg.be>
2024-04-23 16:21:28 +0200

    Merge conflict fix

---
+++ IP_MVC/wwwroot/dist/Views/Flow/Questions/redirectedQuestionId.js
@@ -0,0 +1,6 @@
+function setRedirectedQuestionId(value) {
+    var redirectedQuestionIdInput = document.getElementsByName("redirectedQuestionId")[0];
+    if (redirectedQuestionIdInput) {
+        redirectedQuestionIdInput.value = value;
+    }
+}
