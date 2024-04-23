75631b4aa620940ec41e9668801a6780b7fdd6e3
Stefaan.Vos <stefaan.vos@student.kdg.be>
2024-04-23 16:23:04 +0200

    Merge conflict fix

--- IP_MVC/wwwroot/dist/Views/Flow/Questions/range.ts
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const selectedValue = document.getElementById('selectedValue') as HTMLInputElement;
-    selectedValue.value = selectedValue.dataset.earlierAnswer || '';
-});
-
-document.querySelector('input[type="range"]').addEventListener('input', (e) => {
-    const selectedValue = document.getElementById('selectedValue') as HTMLInputElement;
-    selectedValue.value = (e.target as HTMLInputElement).value;
-});
