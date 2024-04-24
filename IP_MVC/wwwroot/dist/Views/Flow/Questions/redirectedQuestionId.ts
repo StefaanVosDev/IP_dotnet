ea70052ed7f82c1151a15aa5e6f13f38ff0eb9b4
Stefaan.Vos <stefaan.vos@student.kdg.be>
2024-04-24 14:08:18 +0200

    Changing question views

--- IP_MVC/wwwroot/dist/Views/Flow/Questions/redirectedQuestionId.ts
+++
@@ -1,6 +0,0 @@
-function setRedirectedQuestionId(value: string): void {
-    const redirectedQuestionIdInput = document.getElementsByName("redirectedQuestionId")[0] as HTMLInputElement;
-    if (redirectedQuestionIdInput) {
-        redirectedQuestionIdInput.value = value;
-    }
-}
--- IP_MVC/wwwroot/dist/range.entry.js
+++ IP_MVC/wwwroot/dist/range.entry.js
@@ -5,11 +5,12 @@ var __webpack_exports__ = {};
   !*** ./src/ts/Views/Flow/Questions/range.ts ***!
   \**********************************************/
 
+var _a;
 window.addEventListener('DOMContentLoaded', () => {
     const selectedValue = document.getElementById('selectedValue');
     selectedValue.value = selectedValue.dataset.earlierAnswer || '';
 });
-document.querySelector('input[type="range"]').addEventListener('input', (e) => {
+(_a = document.querySelector('input[type="range"]')) === null || _a === void 0 ? void 0 : _a.addEventListener('input', (e) => {
     const selectedValue = document.getElementById('selectedValue');
     selectedValue.value = e.target.value;
 });
