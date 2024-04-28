ec581fb1d417e457455c1af28819f0d70d1010fc
Pepijn Theuns <pepijn.theuns@student.kdg.be>
2024-04-28 15:15:54 +0200

    Vollegige template vaaor popup

--- IP_MVC/ClientApp/src/ts/createFlow.ts
+++ IP_MVC/ClientApp/src/ts/createFlow.ts
@@ -7,38 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const submitButton = document.getElementById('createButton')!;
 
-    // Function to open the popup
-
     function openPopup() {
 
         popupOverlay.style.display = 'block';
 
     }
 
-    // Function to close the popup
 
     function closePopupFunc() {
 
         popupOverlay.style.display = 'none';
 
     }
-
-    // Function to submit the signup form
-
-    // Event listeners
-
-    // Trigger the popup to open (you can call this function on a button click or any other event)
+    
 
     popupButton.addEventListener('click', openPopup);
-
-    // Close the popup when the close button is clicked
-
+    
     closePopup.addEventListener('click', closePopupFunc);
     
     submitButton.addEventListener('click', closePopupFunc);
-
-    // Close the popup when clicking outside the popup content
-
+    
     popupOverlay.addEventListener('click', function (event) {
 
         if (event.target === popupOverlay) {
@@ -48,7 +36,5 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
     });
-
-    // You can customize and expand these functions based on your specific requirements.
-
+    
 });
