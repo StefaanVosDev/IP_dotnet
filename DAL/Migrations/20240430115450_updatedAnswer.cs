7413aa54387c9c5ffd8b11db5b9c00eb242e64a4
Kobe Ponet <kobe.ponet@student.kdg.be>
2024-04-30 15:11:55 +0200

    migrated db for updated answer

---
+++ DAL/Migrations/20240430115450_updatedAnswer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedAnswer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FlowId",
+                table: "Answers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_FlowId",
+                table: "Answers",
+                column: "FlowId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Answers_Flows_FlowId",
+                table: "Answers",
+                column: "FlowId",
+                principalTable: "Flows",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Answers_Flows_FlowId",
+                table: "Answers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Answers_FlowId",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "FlowId",
+                table: "Answers");
+        }
+    }
+}
