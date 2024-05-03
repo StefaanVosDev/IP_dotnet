632572dcaee35523fd826964c2979a55896841d0
Rune van Bossuyt <rune.vanbossuyt@student.kdg.be>
2024-05-03 14:19:50 +0200

    changes

--- IP_MVC/ClientApp/src/ts/Views/Flow/createScroll.ts
+++ IP_MVC/ClientApp/src/ts/Views/Flow/createScroll.ts
@@ -1,15 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var cards = document.querySelectorAll('.swipe');
-    for (var i = 1; i < cards.length; i++) {
-        cards[i].classList.add('hidden');
+let slideIndex: number = 0;
+
+function plusDivs(n: number): void {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n: number): void {
+    let i: number;
+    let x: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("flow-card") as HTMLCollectionOf<HTMLElement>;
+
+    if (x.length === 0) {
+        console.error("Geen elementen gevonden met de klasse naam 'flow-card'");
+        return;
+    }
+
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = x.length;
+    }
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    x[slideIndex - 1].style.display = "block";
+}
+
+document.addEventListener('click', function(event) {
+    if (event.target) {
+        if ((event.target as Element).matches('.w3-display-left')) {
+            plusDivs(-1);
+        } else if ((event.target as Element).matches('.w3-display-right')) {
+            plusDivs(1);
+        }
     }
 });
 
-var currentIndex = 0;
-var cards = document.querySelectorAll('.swipe');
-
-function nextCard() {
-    cards[currentIndex].classList.add('hidden');
-    currentIndex = (currentIndex + 1) % cards.length;
-    cards[currentIndex].classList.remove('hidden');
-}
+window.onload = function () {
+    showDivs(slideIndex);
+};
