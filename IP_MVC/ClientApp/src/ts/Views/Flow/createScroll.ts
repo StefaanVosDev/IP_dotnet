eb4244f35d971f81ad45a9d33b274cd7827e0f94
Rune van Bossuyt <rune.vanbossuyt@student.kdg.be>
2024-05-05 16:40:28 +0200

    'eertse versie subflowpagina'

--- IP_MVC/ClientApp/src/ts/Views/Flow/createScroll.ts
+++ IP_MVC/ClientApp/src/ts/Views/Flow/createScroll.ts
@@ -2,29 +2,24 @@ import Swiper from 'swiper';
 
 document.addEventListener("DOMContentLoaded", function() {
     const swiper = new Swiper(".swiper", {
-        // Default parameters
         slidesPerView: 1,
         loop: true,
         centeredSlides: true,
         spaceBetween: 10,
-        // Navigation arrows
+
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-        // Responsive breakpoints
         breakpoints: {
-            // when window width is >= 320px
             320: {
                 slidesPerView: 1,
                 spaceBetween: 20
             },
-            // when window width is >= 480px
             480: {
                 slidesPerView: 1,
                 spaceBetween: 30
             },
-            // when window width is >= 640px
             640: {
                 slidesPerView: 1,
                 spaceBetween: 40
@@ -32,7 +27,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    // Eventlistener voor de volgende knop
     const nextButton = document.querySelector(".swiper-button-next");
     if (nextButton) {
         nextButton.addEventListener("click", function() {
@@ -40,7 +34,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    // Eventlistener voor de vorige knop
     const prevButton = document.querySelector(".swiper-button-prev");
     if (prevButton) {
         prevButton.addEventListener("click", function() {
