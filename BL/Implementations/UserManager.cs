77efeec580b909431764400dde27b4dea5b279f3
Kobe Ponet <kobe.ponet@student.kdg.be>
2024-05-21 14:58:52 +0200

    added user manager and repository
    (cherry picked from commit 04a9a6a89160e57544accd695e4dc201925aeeb0)

---
+++ BL/Implementations/UserManager.cs
@@ -0,0 +1,13 @@
+using BL.Interfaces;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace BL.Implementations;
+
+public class UserManager(IUserRepository repository) : Manager<IdentityUser>(repository), IUserManager 
+{
+    public IdentityUser GetUserById(string id)
+    {
+        return repository.GetUserById(id);
+    }
+}
