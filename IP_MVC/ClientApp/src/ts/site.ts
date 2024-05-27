a2e6bb0b882db8595d847d130e129977dad5718f
Pepijn Theuns <pepijn.theuns@student.kdg.be>
2024-05-27 22:02:19 +0200

    disign voor open vragen aangepast

--- IP_MVC/ClientApp/src/ts/site.ts
+++ IP_MVC/ClientApp/src/ts/site.ts
@@ -2,6 +2,9 @@ import '@popperjs/core';
 import 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 // Custom CSS imports
 import '../scss/_variables.scss'
 import '../scss/_commonStyles.scss'
@@ -14,8 +17,7 @@ import '../scss/Project/Project.scss'
 import '../scss/Flow/questions/Questions.scss'
 import '../scss/site.scss';
 import '../scss/Analytics/Analytics.scss';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import '../scss/Flow/questions/questionCompleet.scss';
+import '../scss/Flow/questions/openQuestion.scss';
 
 console.log('The \'site\' bundle has been loaded!');
